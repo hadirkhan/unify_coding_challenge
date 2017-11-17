1c0688be13ee6c1b0366d59db539c70f25218f2c
hadirkhan <hadir.khan@outlook.com>
2017-11-16 21:25:09 -0600

    initializing the repository

---
+++ src/id/unify/Main.java
@@ -0,0 +1,11 @@
+package id.unify;
+
+import java.net.HttpURLConnection;
+
+public class Main {
+
+    private static final String URL = "";
+    public static void main(String[] args) {
+        HttpURLConnection http = new Main();
+    }
+}
